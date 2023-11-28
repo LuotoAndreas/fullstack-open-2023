808fbe5d3699de7bec17c886d2a96c718c8a9df4
LuotoAndreas <152082632+LuotoAndreas@users.noreply.github.com>
2023-11-28 20:58:12 +0200

    exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
 
 const Header = ({ header }) => {
   return (
@@ -6,8 +6,29 @@ const Header = ({ header }) => {
   );
 };
 
-const Display = ({ counterG, counterB, counterN,
-  counterAll, counterA, counterP }) => {
+const Statistics = (props) => {
+  const { good, neutral, bad } = props;
+
+  const all = good + neutral + bad;
+  const average = ((good + neutral * 0 + bad * -1) / all).toFixed(1);
+  const positive = ((good / all) * 100).toFixed(1);
+
+  return (
+    <div>
+      <Display
+        counterG={good}
+        counterN={neutral}
+        counterB={bad}
+        counterAll={all}
+        counterA={average}
+        counterP={positive}
+      />
+    </div>
+  );
+};
+
+const Display = ({ counterG, counterB, counterN, 
+                    counterAll, counterA, counterP }) => {
   return (
     <div>
       <p>good {counterG} </p>
@@ -17,65 +38,48 @@ const Display = ({ counterG, counterB, counterN,
       <p>average {counterA} </p>
       <p>positive {counterP}%</p>
     </div>
-  )
-}
+  );
+};
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
   </button>
-)
+);
 
 const App = () => {
+  const header = "give feedback";
+  const header2 = "statistics";
 
-  const header = "give feedback"
-  const header2 = "statistics"
-
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const increaseGood = () => {
-    setGood(good + 1)
-  }
+    setGood(good + 1);
+  };
 
   const increaseNeutral = () => {
-    setNeutral(neutral + 1)  
-  }
+    setNeutral(neutral + 1);
+  };
 
   const increaseBad = () => {
     setBad(bad + 1);
-  }
-
-  const all = (good + neutral + bad)
-  const average = ((good + (neutral * 0) + (bad * (-1))) / all).toFixed(1);
-  const positive = ((good / all) * 100).toFixed(1)
-
+  };
 
   return (
     <div>
       <Header header={header} />
 
-      <Button
-        handleClick={increaseGood}
-        text='good'
-      />
-      <Button
-        handleClick={increaseNeutral}
-        text='neutral'
-      />
-      <Button
-        handleClick={increaseBad}
-        text='bad'
-      />
+      <Button handleClick={increaseGood} text='good' />
+      <Button handleClick={increaseNeutral} text='neutral' />
+      <Button handleClick={increaseBad} text='bad' />
 
       <Header header={header2} />
 
-      <Display counterG={good} counterN={neutral}
-        counterB={bad} counterAll={all} counterA={average}
-        counterP={positive} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
